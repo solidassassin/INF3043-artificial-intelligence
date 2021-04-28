64215aaa78ab6816f1f00a3204edbebe7ba29a13
Rokas Krivaitis <47082246+solidassassin@users.noreply.github.com>
2021-04-28 09:44:36 +0300

    Optimize

--- boolean-logic/resolution.jl
+++ boolean-logic/resolution.jl
@@ -1,24 +1,29 @@
+using BenchmarkTools
+
 const Term = Vector{String}
 
-function evaluate(exp1::Term, exp2::Term)
+@inline function resolution(exp1::Term, exp2::Term)
     for (a, b) = Iterators.product(exp1, exp2)
         if ('¬' * b == a) || ('¬' * a == b)
             temp = filter(i -> i ∉ [a, b], vcat(exp1, exp2)) |> unique
-            if length(temp) != length(unique(strip.(temp, '¬')))
+            if length(temp) != length(unique(lstrip.(temp, '¬')))
                 return Term()
             end
-            return temp
+            return isempty(temp) ? nothing : temp
         end
     end
     Term()
 end
 
-function resolution!(db::Vector{Term})
+function evaluate!(db::Vector{Term})
     len = db |> length
     indexes = collect(2:len)
     for i in indexes
         for j in 1:i
-            contender = evaluate(db[i], db[j])
+            @inbounds contender = resolution(db[i], db[j])
+            if contender === nothing
+                return
+            end
             if !isempty(contender) && !any(db .⊆ (contender,))
                 println(join(contender, " ⊻ "), " ", (i, j))
                 push!(db, contender)
@@ -31,6 +36,5 @@ end
 
 statements = [String.(i) for i = split.(readlines("input2.txt"), " ⊻ ")]
 
-resolution!(statements)
-#@. println(join(statements, " ⊻ "))
+@btime evaluate!(statements)
 
