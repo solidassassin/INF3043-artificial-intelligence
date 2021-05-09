1ad3fde3b2dd586d59278475a9477edf0740290c
Rokas Krivaitis <47082246+solidassassin@users.noreply.github.com>
2021-05-09 16:32:13 +0300

    Homework 2

--- boolean-logic/resolution.jl
+++ boolean-logic/resolution.jl
@@ -1,8 +1,6 @@
-using BenchmarkTools
-
 const Term = Vector{String}
 
-@inline function resolution(exp1::Term, exp2::Term)
+function resolution(exp1::Term, exp2::Term)
     for (a, b) = Iterators.product(exp1, exp2)
         if ('¬' * b == a) || ('¬' * a == b)
             temp = filter(i -> i ∉ [a, b], vcat(exp1, exp2)) |> unique
@@ -16,25 +14,26 @@ const Term = Vector{String}
 end
 
 function evaluate!(db::Vector{Term})
-    len = db |> length
-    indexes = collect(2:len)
-    for i in indexes
+    i = 1
+    parents = fill((0, 0), length(db))
+
+    while (i += 1) <= length(db)
         for j in 1:i
-            @inbounds contender = resolution(db[i], db[j])
+            contender = resolution(db[i], db[j])
             if contender === nothing
-                return
+                return parents
             end
             if !isempty(contender) && !any(db .⊆ (contender,))
-                println(join(contender, " ⊻ "), " ", (i, j))
                 push!(db, contender)
-                push!(indexes, len += 1)
+                push!(parents, (i, j))
             end
         end
     end
+    parents
 end
 
+statements = [String.(i) for i = split.(readlines(ARGS[1]), " ⊻ ")]
+parents = @time evaluate!(statements)
 
-statements = [String.(i) for i = split.(readlines("input2.txt"), " ⊻ ")]
-
-@btime evaluate!(statements)
+println.(1:length(parents), " | ", join.(statements, " ⊻ "), " ", parents)
 
